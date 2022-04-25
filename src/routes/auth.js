6bf215efc8d9ba675f3a54d705a082c662e6e2b1
kartik18g <gupta.kartik18kg@gmail.com>
2022-04-25 18:47:19 +0530

    express validator added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,43 +1,60 @@
-import express from 'express'
-import User from '../services/mongodb/models/User'
-import bcrypt from 'bcryptjs'
+import express from "express";
+import User from "../services/mongodb/models/User";
+import bcrypt from "bcryptjs";
+import { validationResult, body } from "express-validator";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/signup',async(req,res)=>{
+router.post(
+  "/signup",
+  body("firstName").isLength({ min: 1 }),
+  body("lastName").isLength({ min: 1 }),
+  body("password").isLength({ min: 3 }),
+  body("email").isEmail(),
+  async (req, res) => {
     try {
-        const {firstName,lastName,email,password} = req.body;
-        const salt = await bcrypt.genSalt(5);
-        const hashedPassword = await bcrypt.hash(password,salt);
+      const { errors } = validationResult(req);
+      console.log(errors)
+      if(errors.length>0) 
+      return res.json({
+        data: {
+          user:null,
+        },
+        success: false,
+        message: "validation failed",
+      });
 
-        const user = new User({
-          firstName,
-          lastName,
-          email,
-          password: hashedPassword,
-        });
+      const { firstName, lastName, email, password } = req.body;
+      const salt = await bcrypt.genSalt(5);
+      const hashedPassword = await bcrypt.hash(password, salt);
 
-        await user.save()
+      const user = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      });
 
-         res.json({
-           data: {
-             user
-           },
-           success: true,
-           message: "User saved successfully"
-         });
-        console.log(user)
+      await user.save();
 
+      return res.json({
+        data: {
+          user,
+        },
+        success: true,
+        message: "User saved successfully",
+      });
     } catch (error) {
-        console.log(error)
-        res.json({
-          data: {
-            user: null,
-          },
-          success:false,
-          message:error.message
-        });
+      console.log(error);
+      return res.json({
+        data: {
+          user: null,
+        },
+        success: false,
+        message: error.message,
+      });
     }
-})
+  }
+);
 
-export default router
+export default router;
