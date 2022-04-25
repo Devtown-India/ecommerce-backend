bb7a8a21eb4e8f538ef615748b8066f77f1e1271
kartik18g <gupta.kartik18kg@gmail.com>
2022-04-25 19:03:46 +0530

    auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,8 +2,17 @@ import express from "express";
 import User from "../services/mongodb/models/User";
 import bcrypt from "bcryptjs";
 import { validationResult, body } from "express-validator";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+
+/*
+type : POST
+path : /user/signup
+body : {firstName,lastName,email,password}
+query: none
+description: Route to sign up a new user
+*/
 
 router.post(
   "/signup",
@@ -14,15 +23,15 @@ router.post(
   async (req, res) => {
     try {
       const { errors } = validationResult(req);
-      console.log(errors)
-      if(errors.length>0) 
-      return res.json({
-        data: {
-          user:null,
-        },
-        success: false,
-        message: "validation failed",
-      });
+      console.log(errors);
+      if (errors.length > 0)
+        return res.json({
+          data: {
+            user: null,
+          },
+          success: false,
+          message: "validation failed",
+        });
 
       const { firstName, lastName, email, password } = req.body;
       const salt = await bcrypt.genSalt(5);
@@ -49,6 +58,89 @@ router.post(
       return res.json({
         data: {
           user: null,
+        },
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+);
+
+/*
+type : POST
+path : /user/login
+body : {email,password}
+query: none
+description: Route to login a user
+*/
+
+router.post(
+  "/login",
+  body("password").isLength({ min: 3 }),
+  body("email").isEmail(),
+  async (req, res) => {
+    try {
+      const { errors } = validationResult(req);
+      console.log(errors);
+      if (errors.length > 0)
+        return res.json({
+          data: {
+            user: null,
+          },
+          success: false,
+          message: "validation failed",
+        });
+
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user)
+        return res.json({
+          data: {
+            token: null,
+          },
+          success: false,
+          message: "User does not exist",
+        });
+
+      const isVerified = await bcrypt.compare(password, user.password);
+
+      if (!isVerified)
+        return res.json({
+          data: {
+            token: null,
+          },
+          success: false,
+          message: "Invalid password",
+        });
+
+      //   verified user create the JWT
+
+      const token = jwt.sign(
+        {
+          id: user._id,
+          email: user.email,
+          role: user.role,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: "24h" }
+      );
+
+      await user.save();
+
+      return res.json({
+        data: {
+          token,
+        },
+        success: true,
+        message: "User logged in successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.json({
+        data: {
+          token: null,
         },
         success: false,
         message: error.message,
