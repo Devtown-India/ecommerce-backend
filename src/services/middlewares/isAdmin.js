c0eb5bbdeea0d8cdf143c7fd790cbcc90f5d11f0
kartik18g <gupta.kartik18kg@gmail.com>
2022-04-26 19:24:00 +0530

    route protection

--- src/services/middlewares/isAdmin.js
+++ src/services/middlewares/isAdmin.js
@@ -1,20 +1,13 @@
 import { verifyJWT } from "../../utils";
 
 export const isAdmin = (req, res, next) => {
-  try {
-    const token = req.headers["authorization"].split(" ")[1];
-    const {role} = verifyJWT(token);
-    if(role>0) return next()
-    else return res.json({
+  const token = req.headers["authorization"].split(" ")[1];
+  const { role } = verifyJWT(token);
+  if (role > 0) return next();
+  else
+    return res.json({
       data: {},
       success: false,
       message: "Access Denied",
     });
-  } catch (error) {
-    return res.json({
-      data: {},
-      success: false,
-      message: "Unauthorised",
-    });
-  }
 };
