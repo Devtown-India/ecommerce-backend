b267e3f56148fe0db320d98d6496ccc7b35c695a
kartik18g <gupta.kartik18kg@gmail.com>
2022-04-27 19:28:11 +0530

    change

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ app.get('/',(req,res)=>{
     res.send(`Server deployed on PORT ; ${PORT} `);
 })
 
+app.get("/getenvs", (req, res) => {
+  res.send(process.env);
+});
+
+console.log(process.env.DB_URI)
+
 app.listen(PORT,()=>{
     console.log(`Server listening at port ${PORT}`)
 })
